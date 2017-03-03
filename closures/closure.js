39de7e79163acfbe8951b1fc4a5233abdb5d3085
Cheshire <Christopher.Cheshire@denvergov.org>
2017-03-03 11:43:47 -0700

    added closure and global js files

---
+++ closures/closure.js
@@ -0,0 +1,10 @@
+var men = 3;
+
+function countPeople() {
+  var women = 5
+  console.log(women);
+  console.log(men);
+}
+
+console.log(women);
+console.log(men);
---
+++ closures/global.js
@@ -0,0 +1,11 @@
+var count = 0;
+
+function addMan() {
+  count += 1;
+  return count + ' men';
+}
+
+function addWoman() {
+  count += 1;
+  return count + ' women';
+}
